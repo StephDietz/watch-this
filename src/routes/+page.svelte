ad8baabd2fcff887033d511a22f689950cf8a31c
StephDietz <steph.dietz@vercel.com>
2023-01-31 21:36:32 -0600

    streaming

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -56,6 +56,8 @@
 	];
 	let loading = false;
 	let error = '';
+	let recommendationWords = [];
+	let recString = '';
 	/**
 	 * @type {string}
 	 */
@@ -73,27 +75,30 @@
 	 * @param {string} str
 	 */
 	function reformData(str) {
-		let lines = str.split('\n\n');
-		lines.shift();
-		lines = lines;
-		console.log(lines);
-		return lines.map((line) => {
-			// @ts-ignore
-			const [, title, description] = line.match(/\d\.\s*(.*?):\s*(.*)/);
-			return { title, description };
-		});
+		if (str.trim() === '') {
+			return;
+		}
+		const [, title, description] = str.match(/\d\.\s*(.*?):\s*(.*)/);
+		return { title, description };
+		// let lines = str.split('\n\n');
+		// lines.shift();
+		// lines = lines;
+		// console.log(lines);
+		// return lines.map((line) => {
+		// 	// @ts-ignore
+		// 	const [, title, description] = line.match(/\d\.\s*(.*?):\s*(.*)/);
+		// 	return { title, description };
+		// });
 	}
-	// "1. Title: this is the description
-	//
-	//    2. Title 2: Next"
+
 	async function search() {
 		recommendations = [];
 		loading = true;
-		let fullSearchCriteria = `Give me a list of 5 ${cinemaType} recommendations that fit some or all of the following categories: ${selectedCategories}. ${
+		let fullSearchCriteria = `Give me a list of 5 ${cinemaType} recommendations that fit all of the following categories: ${selectedCategories}. ${
 			specificDescriptors
 				? `Make sure it fits the following description as well: ${specificDescriptors}.`
 				: ''
-		}. Please include descriptions of each show as well. `;
+		} Please include descriptions of each show as well. `;
 		const response = await fetch('/api/getRecommendation', {
 			method: 'POST',
 			body: JSON.stringify({ searched: fullSearchCriteria }),
@@ -101,17 +106,37 @@
 				'content-type': 'application/json'
 			}
 		});
-		console.log(response);
-
-		// streaming pseduo code
-		// for (const chunk of stream) {
-		//     recs = reformData(chunk)
-		//     recommendations = recs[recs.length-1]
-		// }
-
 		if (response.ok) {
-			let res = await response.json();
-			recommendations = reformData(res.choices[0].text);
+			const data = response.body;
+			if (!data) {
+				return;
+			}
+
+			const reader = data.getReader();
+			const decoder = new TextDecoder();
+			let done = false;
+
+			while (!done) {
+				const { value, done: doneReading } = await reader.read();
+				done = doneReading;
+				const chunkValue = decoder.decode(value);
+
+				if (chunkValue.trim() === '') {
+					let obj = reformData(recString);
+					if (obj) {
+						recommendations.push(obj);
+						recommendations = recommendations;
+					}
+					recString = '';
+					recommendationWords = [];
+				} else {
+					recommendationWords.push(chunkValue);
+					recommendationWords = recommendationWords;
+					recString = recommendationWords.reduce((acc, val) => acc + val, '');
+				}
+				// if(chunkValue)
+			}
+			// recommendations = reformData(res.choices[0].text);
 		} else {
 			/*
             Possible errors:
@@ -133,59 +158,57 @@
 	<div class="text-center font-bold text-2xl md:text-4xl mb-10">
 		Get curated show or movie recommendations with Open AI
 	</div>
-	<div>
-		<div class="mb-8">
-			<div class="mb-4 font-semibold">What kind of cinema are you searching for?</div>
-			<div>
-				<select class="p-2 rounded-md border text-gray-600 w-full text-sm" bind:value={cinemaType}>
-					<option value="tv show"> TV Show </option>
-					<option value="movie"> Movie </option>
-					<option value="tv show or movie"> No Preference </option>
-				</select>
-			</div>
-		</div>
+	<div class="mb-8">
+		<div class="mb-4 font-semibold">What kind of cinema are you searching for?</div>
 		<div>
-			<div class="mb-4 font-semibold">
-				Select all categories that you want the show or movie to include.
-			</div>
-			<div class="flex items-center flex-wrap">
-				{#each categoryTypes as category}
-					<label class="mr-2 mb-2">
-						<input
-							type="checkbox"
-							bind:group={selectedCategories}
-							name="categories"
-							value={category}
-						/>
-						{category}
-					</label>
-				{/each}
-			</div>
-		</div>
-		<div class="my-8">
-			<div class="mb-4 font-semibold">
-				Write any other specifications here. Be as picky as you'd like.
-			</div>
-			<textarea
-				bind:value={specificDescriptors}
-				class="p-2 rounded-md border text-gray-600 w-full h-20 text-sm"
-				placeholder="Ex. Must have at least 2 seasons and be on Netflix or Hulu."
-			/>
-			<button
-				on:click={search}
-				class="mt-4 w-full h-10 text-white font-bold p-3 rounded bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center"
-			>
-				{#if loading}
-					<LoadingIndicator />
-				{:else}
-					<p>Curate My List</p>
-				{/if}
-			</button>
+			<select class="p-2 rounded-md border text-gray-600 w-full text-sm" bind:value={cinemaType}>
+				<option value="tv show"> TV Show </option>
+				<option value="movie"> Movie </option>
+				<option value="tv show or movie"> No Preference </option>
+			</select>
 		</div>
 	</div>
+	<div>
+		<div class="mb-4 font-semibold">
+			Select all categories that you want the show or movie to include.
+		</div>
+		<div class="flex items-center flex-wrap">
+			{#each categoryTypes as category}
+				<label class="mr-2 mb-2">
+					<input
+						type="checkbox"
+						bind:group={selectedCategories}
+						name="categories"
+						value={category}
+					/>
+					{category}
+				</label>
+			{/each}
+		</div>
+	</div>
+	<div class="my-8">
+		<div class="mb-4 font-semibold">
+			Write any other specifications here. Be as picky as you'd like.
+		</div>
+		<textarea
+			bind:value={specificDescriptors}
+			class="p-2 rounded-md border text-gray-600 w-full h-20 text-sm"
+			placeholder="Ex. Must have at least 2 seasons and be on Netflix or Hulu."
+		/>
+		<button
+			on:click={search}
+			class="mt-4 w-full h-10 text-white font-bold p-3 rounded bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center"
+		>
+			{#if loading}
+				<LoadingIndicator />
+			{:else}
+				<p>Curate My List</p>
+			{/if}
+		</button>
+	</div>
 
-	{#if loading}
-		<div class="fontsemibold text-lg text-center mt-8">
+	{#if loading && !recString}
+		<div class="fontsemibold text-lg text-center mt-8 mb-4">
 			Please be patient as I think. Good things are coming 😎.
 		</div>
 	{/if}
@@ -195,8 +218,8 @@
 		</div>
 	{/if}
 	{#if recommendations}
-		{#each recommendations as recommendation}
-			<div class="mb-4">
+		{#each recommendations as recommendation, i (i)}
+			<div class="mb-4 rounded-lg shadow bg-white p-4">
 				<div class="text-2xl font-bold mb-2">
 					{recommendation.title}
 				</div>
@@ -206,4 +229,7 @@
 			</div>
 		{/each}
 	{/if}
+	<div>
+		{recString}
+	</div>
 </div>
--- src/routes/api/getRecommendation/+server.ts
+++ src/routes/api/getRecommendation/+server.ts
@@ -1,7 +1,8 @@
 import { error } from '@sveltejs/kit';
+import { OpenAIStream } from '../OpenAIStream';
 
-const endpoint = 'https://api.openai.com/v1/completions';
-const key = import.meta.env.VITE_OPENAI_API_KEY;
+// const endpoint = 'https://api.openai.com/v1/completions';
+// const key = import.meta.env.VITE_OPENAI_API_KEY;
 
 export async function POST({ request }) {
 	const { searched } = await request.json();
@@ -12,15 +13,18 @@ export async function POST({ request }) {
 		max_tokens: 2048,
 		top_p: 1.0,
 		frequency_penalty: 0.0,
-		presence_penalty: 0.0
+		stream: true,
+		presence_penalty: 0.0,
+		n: 1
 	};
-
-	return await fetch(endpoint, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${key}`
-		},
-		body: JSON.stringify(payload)
-	});
+	const stream = await OpenAIStream(payload);
+	return new Response(stream);
+	// return await fetch(endpoint, {
+	// 	method: 'POST',
+	// 	headers: {
+	// 		'Content-Type': 'application/json',
+	// 		Authorization: `Bearer ${key}`
+	// 	},
+	// 	body: JSON.stringify(payload)
+	// });
 }
