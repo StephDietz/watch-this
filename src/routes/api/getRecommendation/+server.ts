c3f2fc6826ec227fbb5a5e1d2c1df38e13f2a35d
StephDietz <steph.dietz@vercel.com>
2023-02-10 10:50:02 -0600

    bug fix

--- src/routes/api/getRecommendation/+server.ts
+++ src/routes/api/getRecommendation/+server.ts
@@ -17,8 +17,6 @@ async function OpenAIStream(payload: OpenAIStreamPayload) {
 	const encoder = new TextEncoder();
 	const decoder = new TextDecoder();
 
-	let counter = 0;
-
 	const res = await fetch('https://api.openai.com/v1/completions', {
 		headers: {
 			'Content-Type': 'application/json',
@@ -42,13 +40,8 @@ async function OpenAIStream(payload: OpenAIStreamPayload) {
 						const json = JSON.parse(data);
 						const text = json.choices[0].text;
 
-						if (counter < 2 && (text.match(/\n/) || []).length) {
-							// this is a prefix character (i.e., "\n\n"), do nothing
-							return;
-						}
 						const queue = encoder.encode(text);
 						controller.enqueue(queue);
-						counter++;
 					} catch (e) {
 						controller.error(e);
 					}
